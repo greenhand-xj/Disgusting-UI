ee2d735f4938fcb5814f5a06e019cf5115ada1df
徐杰 <2275817182@qq.com>
2023-01-15 17:53:38 +0800

    chore: 配置别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@components': resolve(__dirname, 'src/components'),
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })
